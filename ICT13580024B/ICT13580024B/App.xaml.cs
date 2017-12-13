a85d1e3949ae3a18eeda1b5554aa7a81d9a2e398
13580024 <nutchanon-mark@hotmail.com>
2017-12-13 11:59:33 +0700

    Init

--- ICT13580024B/ICT13580024B/App.xaml.cs
+++ ICT13580024B/ICT13580024B/App.xaml.cs
@@ -13,7 +13,24 @@ namespace ICT13580024B
         {
             InitializeComponent();
 
-            MainPage = new ICT13580024B.MainPage();
+            var np = new NavigationPage(new NavPage1());
+
+            var tp = new TabbedPage();
+            tp.Children.Add(new TabPage1());
+            tp.Children.Add(new TabPage2());
+            tp.Children.Add(new TabPage3());
+
+            var cp = new CarouselPage();
+            cp.Children.Add(new TabPage1());
+            cp.Children.Add(new TabPage2());
+            cp.Children.Add(new TabPage3());
+
+            var mp = new MasterDetailPage();
+            mp.Master = new MenuPage();
+            mp.Detail = new NavigationPage(new ICT13580024BPage());
+
+            MainPage = mp;
+
         }
 
         protected override void OnStart()
--- ICT13580024B/ICT13580024B/TabPage1.xaml.cs
+++ ICT13580024B/ICT13580024B/TabPage1.xaml.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace ICT13580024B
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabPage1 : ContentPage
     {
-        public MainPage()
+        public TabPage1()
         {
             InitializeComponent();
         }
